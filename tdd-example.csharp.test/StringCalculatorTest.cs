98be58b9917b6e5a9ad9f041c967569d4b60a8fa
amorscher <am.morscher@gmail.com>
2024-02-12 22:42:50 +0100

    implement fifth requirement

--- tdd-example.csharp.test/StringCalculatorTest.cs
+++ tdd-example.csharp.test/StringCalculatorTest.cs
@@ -41,10 +41,10 @@ public class StringCalculatorTest
         //GIVEN
         var itemUnderTest = new StringCalculator();
         //WHEN
-        var result =itemUnderTest.Add("1,2,3,4,5");
+        var result = itemUnderTest.Add("1,2,3,4,5");
 
         //THEN
-        Assert.Equal(1+2+3+4+5,result);
+        Assert.Equal(1 + 2 + 3 + 4 + 5, result);
 
     }
 
@@ -73,8 +73,8 @@ public class StringCalculatorTest
         var result3Plus2 = itemUnderTest.Add("3,2");
 
         //THEN
-        Assert.Equal(1+2, result1Plus2);
-        Assert.Equal(3+2, result3Plus2);
+        Assert.Equal(1 + 2, result1Plus2);
+        Assert.Equal(3 + 2, result3Plus2);
 
     }
 
@@ -115,7 +115,83 @@ public class StringCalculatorTest
         var result = itemUnderTest.Add("1\n2\n3");
 
         //THEN
-        Assert.Equal(1+2+3, result);
+        Assert.Equal(1 + 2 + 3, result);
+    }
+
+    [Fact]
+    public void TestThatNewlineAndCommaCanBeUsedAsDelimiter()
+    {
+        //GIVEN
+        var itemUnderTest = new StringCalculator();
+
+        //WHEN
+        var result = itemUnderTest.Add("1\n2,3");
+
+        //THEN
+        Assert.Equal(1 + 2 + 3, result);
+    }
+
+
+
+    [Fact]
+    public void TestThatSingleMixedDelimitersCanBeUsedAsDelimiter()
+    {
+        //GIVEN
+        var itemUnderTest = new StringCalculator();
+
+        //WHEN
+        var resultSemicolon = itemUnderTest.Add("//;\n1;2;3");
+        var resultPipe = itemUnderTest.Add("//|\n1|2|3");
+
+        //THEN
+        Assert.Equal(1 + 2 + 3, resultSemicolon);
+        Assert.Equal(1 + 2 + 3, resultPipe);
+    }
+
+    [Fact]
+    public void TestThatMultiDelimitersCanBeUsedAsDelimiter()
+    {
+        //GIVEN
+        var itemUnderTest = new StringCalculator();
+
+        //WHEN
+        var result = itemUnderTest.Add("//Sep\n1Sep2Sep3");
+
+        //THEN
+        Assert.Equal(1 + 2 + 3, result);
+    }
+
+
+    [Fact]
+    public void TestThatUsingWrongSingleDelimiterThrowsException()
+    {
+        //GIVEN
+        var itemUnderTest = new StringCalculator();
+
+        //WHEN
+        void action() => itemUnderTest.Add("//|\n1|2,3");
+
+        //THEN
+        ArgumentException exception = Assert.Throws<ArgumentException>(action);
+        Assert.Equal("'|' expected but ',' found at position 3.", exception.Message);
+        
+
+    }
+
+        [Fact]
+    public void TestThatUsingWrongMultiDelimiterThrowsException()
+    {
+        //GIVEN
+        var itemUnderTest = new StringCalculator();
+
+        //WHEN
+        void action() => itemUnderTest.Add("//Sep\n1Sep2,3");
+
+        //THEN
+        ArgumentException exception = Assert.Throws<ArgumentException>(action);
+        Assert.Equal("'Sep' expected but ',' found at position 5.", exception.Message);
+        
+
     }
 
 }
--- tdd-example.csharp/src/StringCalculator.cs
+++ tdd-example.csharp/src/StringCalculator.cs
@@ -8,6 +8,7 @@ using System.Text.RegularExpressions;
 public class StringCalculator
 {
 
+
     static void Main(string[] args)
     {
         // is empty for our example
@@ -25,12 +26,76 @@ public class StringCalculator
 
     private List<int> ParseInput(string input)
     {
-        if(input == ""){
-            return new List<int>(1){0};
+        if (input == "")
+        {
+            return new List<int>(1) { 0 };
         }
-        
-        var numbersArray = Regex.Split(input,@",|\n");
-        
+
+        var delimiterSeparatedInput = this.ParseDelimiter(input);
+        var numbersArray = Regex.Split(delimiterSeparatedInput.Numbers, @"" + delimiterSeparatedInput.EscapedDelimiter);
+
+        return this.ParseNumbers(numbersArray);
+
+    }
+    record DelimiterSeparatedInput
+    {
+        public string EscapedDelimiter = "";
+        public string Delimiter = "";
+        public string Numbers = "";
+    }
+    private DelimiterSeparatedInput ParseDelimiter(string input)
+    {
+        if (input.StartsWith("//"))
+        {
+            var splittedInput = input.Split("\n");
+            var delimiterPart = splittedInput[0];
+            var numberPart = splittedInput[1];
+            var delimiter = delimiterPart.Replace("//", "");
+
+            var delimiterSeparatedInput = new DelimiterSeparatedInput
+            {
+                EscapedDelimiter = Regex.Escape(delimiter),
+                Delimiter = delimiter,
+                Numbers = numberPart
+            };
+
+            this.CheckForValidDelimiters(delimiterSeparatedInput.Numbers, delimiterSeparatedInput.Delimiter);
+
+            return delimiterSeparatedInput;
+        }
+
+
+
+
+        return new DelimiterSeparatedInput
+        {
+            EscapedDelimiter = ",|\n",
+            Delimiter = ",|\n",
+            Numbers = input
+        };
+    }
+
+    private void CheckForValidDelimiters(string numbers, string delimiter)
+    {
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            var actChar = numbers[i];
+            if (!char.IsDigit(actChar))
+            {
+                if (i + delimiter.Length > numbers.Length || numbers.Substring(i, delimiter.Length) != delimiter)
+                {
+                    throw new ArgumentException($"'{delimiter}' expected but '{actChar}' found at position {i}.");
+                }
+                else
+                {
+                    i = i + delimiter.Length - 1;
+                }
+            }
+        }
+    }
+
+    private List<int> ParseNumbers(string[] numbersArray)
+    {
         List<int> numberList = new();
         foreach (var item in numbersArray)
         {
@@ -47,6 +112,5 @@ public class StringCalculator
         return numberList;
 
     }
-
 
 }
